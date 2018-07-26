0903f744da16a2c27065fe2a72fec58eaafabe98
sis0k0 <stanimira.vlaeva@gmail.com>
2018-07-26 23:14:14 +0300

    add credits component

---
+++ src/app/credits/credits.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gtm-credits',
+  templateUrl: './credits.component.html',
+  styleUrls: ['./credits.component.css']
+})
+export class CreditsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
