25bb3b4f29f6581675f4f9e3f31450790255fe28
sis0k0 <stanimira.vlaeva@gmail.com>
2018-07-26 23:42:44 +0300

    ng add nativescript

---
+++ src/app/app.component.tns.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gtm-root',
+  templateUrl: './app.component.html',
+})
+
+export class AppComponent { }
---
+++ src/app/app.module.tns.ts
@@ -0,0 +1,35 @@
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
+import { AppRoutingModule } from './app-routing.module.tns';
+import { AppComponent } from './app.component';
+
+import { BarcelonaModule } from './barcelona/barcelona.module';
+
+// Uncomment and add to NgModule imports if you need to use two-way binding
+// import { NativeScriptFormsModule } from "nativescript-angular/forms";
+
+// Uncomment and add to NgModule imports  if you need to use the HTTP wrapper
+// import { NativeScriptHttpClientModule } from 'nativescript-angular/http-client';
+
+@NgModule({
+  bootstrap: [
+    AppComponent
+  ],
+  imports: [
+    NativeScriptModule,
+    AppRoutingModule,
+    BarcelonaModule
+  ],
+  declarations: [
+    AppComponent
+  ],
+  providers: [
+  ],
+  schemas: [
+    NO_ERRORS_SCHEMA
+  ]
+})
+/*
+Pass your application module to the bootstrapModule function located in main.ts to start your app
+*/
+export class AppModule { }
