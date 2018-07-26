074d6901a3dbbb1bf47dd75075dc43bbbde7277e
sis0k0 <stanimira.vlaeva@gmail.com>
2018-07-27 00:19:14 +0300

    migrate credits component to mobile

--- src/app/app.module.tns.ts
+++ src/app/app.module.tns.ts
@@ -1,6 +1,7 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
 import { AppComponent } from './app.component';
+import { CreditsComponent } from './credits/credits.component';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
@@ -16,7 +17,8 @@ import { AppComponent } from './app.component';
     NativeScriptModule,
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    CreditsComponent
   ],
   providers: [
   ],
