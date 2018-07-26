389a56557c2aebb7df517260723bf509499ea275
sis0k0 <stanimira.vlaeva@gmail.com>
2018-07-26 23:36:02 +0300

    remove extra comma from angular.json

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Movie } from 'src/app/movie';
+import { Movie } from '../movie';
 
 @Component({
   selector: 'gtm-game',
